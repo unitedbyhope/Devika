a218201bf0bb7e88d6ed2532cea1aadcaa019a98
unitedbyhope <goyalmayanksingh@gmail.com>
2025-09-04 15:48:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = "Media_search"
 API_ID = int(6129258)
 API_HASH = "9e4430929d7757512f864d160dac0a98"
-BOT_TOKEN = "5030500439:AAHL9P_-PzvqV5pQ_GWCE26IlU4B25ewe2Y"
+BOT_TOKEN = "8209654006:AAGKrI__hgDd7Ds-wF1KxK0k2mVSlqpPy7I"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -22,23 +22,23 @@ USE_CAPTION_FILTER = True
 PICS = (environ.get('PICS', 'https://telegra.ph/file/286801e61b556c4be376a.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '983961765 5208586715 5594675652').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001792180012 -1001570289337 -1001529183313').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5002595907 7972120443').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002587791919').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = int(environ.get('AUTH_CHANNEL', '-1002917042651'))
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://Hemanth:Hemanth@cluster0.uzzh3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-DATABASE_NAME = "cluster0"
+DATABASE_URI = "mongodb+srv://goyalmayanksingh_db_user:H0yWnYSQRqaJN15U@cluster0.wkeavau.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = "Telegram_files"
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001792180012'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Filmcornerofficialgroup')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002419190495'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'epubpdfaudiobooks')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
